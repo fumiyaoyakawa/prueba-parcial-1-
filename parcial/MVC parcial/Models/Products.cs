e8f0d301a449cbafc5a423151878d2c252b02192
mauricio alejandro cuevas vidaurre <reymauricoo@gmail.com>
2019-03-30 12:32:49 -0400

    commit con modelo mvc

---
+++ parcial/MVC parcial/Models/Products.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MVC_parcial.Models
+{
+    public enum UniType
+    {
+        Botella, 
+        Metro, 
+        Litro,
+        Caja,
+    }
+    public enum StatusType
+    {
+        Activo,
+        Inactivo
+    }
+    public class Products
+    {
+        [Key]
+        public int ProductId { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+        [Required]
+        public UniType Unit { get; set; }
+        public StatusType Status { get; set; }
+    }
+}
